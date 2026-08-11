b699e23071f5e424d53d5edae5fae7c6bd38cd8e
Blas Avitia <madaza.avitia@gmail.com>
2026-08-11 13:54:11 -0600

    Crear primer script

---
+++ Scripts/prueba.R
@@ -0,0 +1,19 @@
+# Blas Avitia Arellano
+# 2327168
+# 11/08/2026
+
+
+# Ingresar datos -----------------
+
+altura <- 12
+12 + altura
+altura * altura
+
+# Conjunto de datos
+
+dbh <- c(12, 10, 11.5, 13.2, 18.5)
+altura +dbh
+
+
+
+# Estimar medias -----------------
